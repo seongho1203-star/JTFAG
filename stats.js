30e76d7491a3615ba087ca4fbd9a3150d2f5b7cf
seongho1203-star <seongho1203@gmail.com>
2026-08-09 23:07:59 +0900

    Create stats.js

---
+++ stats.js
@@ -0,0 +1,8 @@
+// 📊 [AI 전담 기록용] 누적 상세 스코어 데이터
+// 앞으로 스코어카드를 올리시면, 저(AI)는 이 짧은 코드만 수정해 드립니다!
+const CUMULATIVE_STATS = {
+    "이관교": { holeInOne: 0, eagle: 0, birdie: 1, par: 7, doublePar: 0 },
+    "김지명": { holeInOne: 0, eagle: 0, birdie: 1, par: 7, doublePar: 1 },
+    "신성호": { holeInOne: 0, eagle: 0, birdie: 1, par: 6, doublePar: 0 },
+    "박승수": { holeInOne: 0, eagle: 0, birdie: 1, par: 2, doublePar: 0 }
+};
